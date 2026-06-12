fd9325edb3e40c420edbaf2d87bea3f18d4ebd05
antapp-cc <zhangzhelei@gmail.com>
2026-06-12 20:48:02 +0800

    Update sources-auto.sh

--- sources-auto.sh
+++ sources-auto.sh
@@ -4,8 +4,13 @@ set -e
 . /etc/os-release
 
 case "${VERSION_CODENAME:-}" in
-  bullseye|bookworm|trixie)
+  bullseye)
+    codename="bullseye"
+    components="main contrib non-free"
+    ;;
+  bookworm|trixie)
     codename="${VERSION_CODENAME}"
+    components="main contrib non-free non-free-firmware"
     ;;
   *)
     echo "不支持的 Debian 版本: ${PRETTY_NAME:-unknown}" >&2
@@ -16,9 +21,9 @@ esac
 cp /etc/apt/sources.list /etc/apt/sources.list.backup.$(date +%Y%m%d%H%M%S) 2>/dev/null || true
 
 cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian ${codename} main contrib non-free non-free-firmware
-deb http://deb.debian.org/debian ${codename}-updates main contrib non-free non-free-firmware
-deb http://deb.debian.org/debian-security ${codename}-security main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian ${codename} ${components}
+deb http://deb.debian.org/debian ${codename}-updates ${components}
+deb http://deb.debian.org/debian-security ${codename}-security ${components}
 EOF
 
 apt-get clean
